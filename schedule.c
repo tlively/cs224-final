c2d6c6669c6f78f6743709dc6a4470d1119ac8f0
Thomas Lively <tlively123@gmail.com>
2017-05-04 19:02:35 -0400

    Implement calculation of schedule length

--- schedule.c
+++ schedule.c
@@ -1,4 +1,7 @@
+#include <assert.h>
+#include <limits.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "vector.h"
 #include "bitmap.h"
@@ -12,29 +15,119 @@ struct schedule {
 };
 
 schedule *schedule_create(dag *g, unsigned m) {
-    // TODO
+    assert(g != NULL);
+    assert(m > 0);
+    schedule *s = malloc(sizeof(*s));
+    if (s == NULL) {
+        return NULL;
+    }
+    if (idx_vec_init(&s->order, 0) != 0) {
+        free(s);
+        return NULL;
+    }
+    s->g = g;
+    s->m = m;
+    return s;
 }
 
 void schedule_destroy(schedule *s) {
-    // TODO
+    assert(s != NULL);
+    idx_vec_destroy(&s->order);
+    free(s);
 }
 
-void schedule_add(schedule *s, unsigned idx) {
-    // TODO
+int schedule_add(schedule *s, unsigned idx) {
+    assert(s != NULL);
+    assert(idx < dag_size(s->g));
+    assert(s->order.size < dag_size(s->g) - 1);
+    return idx_vec_push(&s->order, idx);
 }
 
-void schedule_pop(schedule *s) {
-    // TODO
+int schedule_pop(schedule *s) {
+    assert(s != NULL);
+    assert(s->order.size > 0);
+    return idx_vec_pop(&s->order, NULL);
+}
+
+size_t schedule_size(schedule *s) {
+    assert(s != NULL);
+    return s->order.size;
+}
+
+int schedule_is_complete(schedule *s) {
+    assert(s != NULL);
+    return s->order.size == dag_size(s->g);
 }
 
 int schedule_validate(schedule *s) {
+    return 0;
     // TODO
 }
 
 int schedule_length(schedule *s) {
-    // TODO
+    assert(s != NULL);
+    int end_times[s->m];
+    int cur_items[s->m];
+    memset(end_times, 0, s->m * sizeof(int));
+    memset(cur_items, 0, s->m * sizeof(int));
+    int preds_done[dag_size(s->g)];
+    memset(preds_done, 0, dag_size(s->g) * sizeof(int));
+    for (size_t i = 0; i < s->order.size; i++) {
+        unsigned idx = s->order.data[i];
+        unsigned midx = (unsigned) -1;
+        // schedule immediately
+        if (preds_done[idx] == dag_npreds(s->g, idx)) {
+            int min_end_time = INT_MAX;
+            for (size_t m = 0; m < s->m; m++) {
+                if (end_times[m] < min_end_time) {
+                    min_end_time = end_times[m];
+                    midx = m;
+                }
+            }
+        }
+        // or after dependencies finish
+        else {
+            bitmap *dep_set = bitmap_create(dag_size(s->g));
+            if (dep_set == NULL) {
+                return -1;
+            }
+            size_t ndeps = dag_npreds(s->g, idx);
+            unsigned deps[ndeps];
+            dag_preds(s->g, idx, deps);
+            for (size_t i = 0; i < ndeps; i++) {
+                bitmap_set(dep_set, deps[i], 1);
+            }
+            int max_end_time = INT_MIN;
+            for (size_t m = 0; m < s->m; m++) {
+                if (bitmap_get(dep_set, cur_items[m]) == 1 &&
+                    end_times[m] > max_end_time) {
+                    max_end_time = end_times[m];
+                    midx = m;
+                }
+            }
+            assert(max_end_time != INT_MIN);
+            bitmap_destroy(dep_set);
+        }
+        assert(midx >= 0 && midx < s->m);
+        end_times[midx] += dag_weight(s->g, idx);
+        cur_items[midx] = idx;
+        size_t nsuccs = dag_nsuccs(s->g, idx);
+        unsigned succs[nsuccs];
+        dag_succs(s->g, idx, succs);
+        for (size_t i = 0; i < nsuccs; i++) {
+            preds_done[i]++;
+        }
+    }
+    int max_end_time = INT_MIN;
+    for (size_t m = 0; m < s->m; m++) {
+        if (end_times[m] > max_end_time) {
+            max_end_time = end_times[m];
+        }
+    }
+    return max_end_time;
 }
 
 int schedule_lower_bound(schedule *s) {
     // TODO
+    return 0;
 }
--- schedule.h
+++ schedule.h
@@ -10,12 +10,27 @@
 struct schedule;
 typedef struct schedule schedule;
 
+// create an initially empty schedule with the give precedence graph
+// and number of processors. The graph is borrowed, not owned, by the
+// schedule.
 schedule *schedule_create(dag *g, unsigned m);
+
+// releases resources associated with the schedule. Does not destroy
+// the dependency graph it contains.
 void schedule_destroy(schedule *s);
-void schedule_add(schedule *s, unsigned idx);
-void schedule_pop(schedule *s);
+
+// add or remove an item at the end of the schedule.
+int schedule_add(schedule *s, unsigned idx);
+int schedule_pop(schedule *s);
+
+// returns the number of items in the schedule.
+size_t schedule_size(schedule *s);
+
+// returns 1 if the schedule is complete, 0 otherwise.
+int schedule_is_complete(schedule *s);
+
 int schedule_validate(schedule *s);
 int schedule_length(schedule *s);
 int schedule_lower_bound(schedule *s);
 
-#endif SCHEDULE_H
+#endif // SCHEDULE_H
