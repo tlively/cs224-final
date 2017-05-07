af80aa429741bccd47059a745b674f81e1bc4c4e
Thomas Lively <tlively123@gmail.com>
2017-05-07 12:06:10 -0400

    Add experiment for 100 vertex DAGS

--- series.py
+++ series.py
@@ -2,18 +2,21 @@ import subprocess
 import sys
 
 n_dags = 30
-machines = [4,8,16]
+small_machines = [4,8,16]
 timeout = 60
 timeout_skip = 12
+bounds = {"Fujita": [],
+          "Fernandez": ["FB=1"]}
 
-for bound in ["Fujita", "Fernandez"]:
+def make_clean():
     clean = subprocess.run(["make", "clean"],
-                          stdout=subprocess.DEVNULL,
-                          stderr=subprocess.DEVNULL)
+                           stdout=subprocess.DEVNULL,
+                           stderr=subprocess.DEVNULL)
     if clean.returncode != 0:
         print("make clean failed")
         sys.exit(1)
-    args = ["FB=1"] if bound == "Fernandez" else []
+
+def make(args=[]):
     make = subprocess.run(["make"] + args,
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE,
@@ -21,19 +24,52 @@ for bound in ["Fujita", "Fernandez"]:
     if make.returncode != 0:
         print("make failed")
         sys.exit(1)
-    for size in range(12,26):
+
+def bbexps(path, m, t):
+    result = subprocess.run(
+        ["./bbexps", path, str(m), str(t)],
+        stdout=subprocess.PIPE,
+        encoding="utf-8")
+    return result
+
+
+def runSeries():
+    n_dags = 30
+    machines = [4,8,16]
+    timeout_skip = 12
+    sizes = list(range(12,26))
+    for bound in bounds:
+        make_clean()
+        make(bounds[bound])
+        for size in sizes:
+            for m in machines:
+                n_timeouts = 0
+                for dag in range(n_dags):
+                    path = "series/data{}01/Pat{}.rcp".format(size, dag)
+                    result = bbexps(path, m, timeout)
+                    print("{}, {}".format(result.stdout[:-1], bound))
+                    if "-2" in result.stdout:
+                        n_timeouts += 1
+                    else:
+                        n_timeouts = 0
+                    if n_timeouts >= timeout_skip:
+                        break
+
+def run100():
+    n_dags = 100
+    machines = [16, 20, 24, 28, 32]
+    for bound in bounds:
+        make_clean()
+        make(bounds[bound])
         for m in machines:
-            n_timeouts = 0
             for dag in range(n_dags):
-                path = "series/data{}01/Pat{}.rcp".format(size, dag)
-                result = subprocess.run(
-                    ["./bbexps", path, str(m), str(timeout)],
-                    stdout=subprocess.PIPE,
-                    encoding="utf-8")
+                path = "data100/Pat{}.rcp".format(dag)
+                result = bbexps(path, m, timeout)
                 print("{}, {}".format(result.stdout[:-1], bound))
-                if "-2" in result.stdout:
-                    n_timeouts += 1
-                else:
-                    n_timeouts = 0
-                if n_timeouts >= timeout_skip:
-                    break
+
+def main():
+    run100()
+    # runSeries()
+
+if __name__ == "__main__":
+    main()
